84650b89602d4db21349d9491134184f4f696a22
zow1e <zowieong.zo@gmail.com>
2026-01-23 02:24:14 +0800

    Finish Level-1: echo user inputs

--- src/main/java/Kiwi.java
+++ src/main/java/Kiwi.java
@@ -1,10 +1,32 @@
+
+import java.util.Scanner;
+
 public class Kiwi {
     public static void main(String[] args) {
         String chatbotName = "Kiwi";
 
+        // chatbot intro
         System.out.println("Hello! I'm " + chatbotName);
         System.out.println("What can I do for you? \n");
-        System.out.println("\nByebye. Hope to see you again soon!");
+
+
+        // echo user input
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        while (scanner.hasNextLine()) {
+            input = scanner.nextLine().trim();
+
+            if (input.toLowerCase().equals("bye")) {
+                // chatbot exit
+                System.out.println("\nByebye. Hope to see you again soon!");
+                break;
+            } else {
+                // echo the input
+                System.out.println(input + "\n");
+            }
+        }
+        scanner.close();
     }
 }
 
