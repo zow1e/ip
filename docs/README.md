eefdc8381ed4d630448eba69279455e6c2325c4c
zow1e <zowieong.zo@gmail.com>
2026-01-23 02:11:01 +0800

    Finish Level-0: rename Duke to Kiwi, add greeting/exit

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Kiwi User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Kiwi.java
@@ -0,0 +1,10 @@
+public class Kiwi {
+    public static void main(String[] args) {
+        String chatbotName = "Kiwi";
+
+        System.out.println("Hello! I'm " + chatbotName);
+        System.out.println("What can I do for you? \n");
+        System.out.println("\nByebye. Hope to see you again soon!");
+    }
+}
+
