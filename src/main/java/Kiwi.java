8b34ae8ae08b83a6d2cfcd0ca07e5258193f069f
zow1e <zowieong.zo@gmail.com>
2026-01-23 13:56:59 +0800

    Finish Level-3: Add marking function, Create Task class

--- src/main/java/Kiwi.java
+++ src/main/java/Kiwi.java
@@ -3,6 +3,9 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Kiwi {
+
+    // store tasks; no more than 100 tasks
+    private static ArrayList<Task> tasks = new ArrayList<>();
     public static void main(String[] args) {
         String chatbotName = "Kiwi";
 
@@ -14,32 +17,103 @@ public class Kiwi {
         // echo user input
         Scanner scanner = new Scanner(System.in);
         String input;
-        // no more than 100 tasks
-        ArrayList<String> fullList = new ArrayList<>();
 
         while (scanner.hasNextLine()) {
             input = scanner.nextLine().trim();
+            String[] parts = input.split("\\s+", 2);
 
-            if (input.toLowerCase().equals("bye")) {
-                // chatbot exit
-                System.out.println("\nByebye. Hope to see you again soon!");
-                break;
 
-            // list items added
-            } else if (input.toLowerCase().equals("list")) {
-                for (int i=1; i<= fullList.size(); i++) {
-                    System.out.println(i + ". " + fullList.get(i-1));
-                }
-                System.out.println("\n");
+            switch (parts[0].toLowerCase()) {
+                // exit
+                case "bye":
+                    System.out.println("\nByebye. Hope to see you again soon!");
+                    break;
+                
+                // list out items
+                case "list":
+                    listTasks();
+                    break;
+                
+                // mark item as done
+                case "mark":
+                    if (tasks.isEmpty()) {
+                        System.out.println("No items in list");
+                    } else {
+                        int itemIndex = Integer.parseInt(parts[1]);
+                        Task thisItem = tasks.get(itemIndex-1);
+                        thisItem.markTask();
+                        System.out.println("Great! Marked this as done!!\n"+thisItem.toString()+"\n");
+                    }
+                    break;
+                
+                // unmark item
+                case "unmark":
+                    if (tasks.isEmpty()) {
+                        System.out.println("No items in list");
+                    } else {
+                        int itemIndex = Integer.parseInt(parts[1]);
+                        Task thisItem = tasks.get(itemIndex-1);
+                        thisItem.markTask();
+                        System.out.println("ok... marked this as not done...\n"+thisItem.toString()+"\n");
+                    }
+                    break;
+                
+                // ToDo task
+                case "todo":
+                    addTask(new ToDo(parts[1]));
+                    break;
+                // Deadline task
+                case "deadline":
+                    addDeadline(parts[1]);
+                    break;
+                //Event task
+                case "event":
+                    addEvent(parts[1]);
+                    break;
 
-            } else {
-                // add input to list
-                fullList.add(input);
-                // echo the input
-                System.out.println("added: " + input + "\n");
+            
+                default:
+                    System.out.println("Command not recognised!");    
+                    break;
             }
         }
         scanner.close();
+        
     }
+
+
+    private static void addTask(Task task) {
+        tasks.add(task);
+        System.out.println("Added: "+task.toString());
+        System.out.println("There are now "+tasks.size()+" tasks in the list\n");
+    }
+
+    private static void addDeadline(String input) {
+        String[] fullLine = input.split(" /by ");
+        String desc = fullLine[0].trim();
+        String dueDate = fullLine[1].trim();
+        addTask(new Deadline(desc, dueDate));
+    }
+    private static void addEvent(String input) {
+        String[] fullLine = input.split(" /from | /to");
+        String desc = fullLine[0].trim();
+        String from = fullLine[1].trim();
+        String to = fullLine[2].trim();
+        addTask(new Event(desc, from, to));
+    }
+
+    private static void listTasks() {
+        if (tasks.isEmpty()) {
+            System.out.println("No tasks yet!");
+        } else {
+            System.out.println("Here are your tasks:");
+            for (int i = 0; i < tasks.size(); i++) {
+                Task currItem = tasks.get(i);
+                System.out.println((i + 1 )+ ". " + currItem.toString());
+            }
+        }
+        System.out.println("\n");
+    }
+
 }
 
---
+++ src/main/java/Task.java
@@ -0,0 +1,33 @@
+public class Task {
+    
+    protected String description;
+    protected boolean isDone;
+    protected String date;
+
+    // constructor
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    // display task status
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    // mark as done 
+    public void markTask() {
+        this.isDone = true;
+    }
+
+    // mark as not done 
+    public void unmarkTask() {
+        this.isDone = false;
+    }
+    
+    // display description
+    public String toString() {
+        String fullDesc = "[" + getStatusIcon() + "] " + description.toString();
+        return fullDesc;
+    }
+}
