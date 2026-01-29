9da75bd5294d6db94780fdbe66db2b0fc99083bf
zow1e <zowieong.zo@gmail.com>
2026-01-30 00:08:13 +0800

    Set date time input constraints

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,22 +1,32 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task{
-    private String from;
-    private String to;
+    
+    protected String from;
+    protected String to;
 
     public Event(String description, String from, String to) {
         super(description);
-        this.from = from;
+        
+        this.dateTime = LocalDateTime.parse
+                       (from, DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm"));
+        this.from = from.split(" ")[1];
         this.to = to;
     }
 
     public String getFrom() {
         return this.from;
     }
+
     public String getTo() {
         return this.to;
     }
 
+
     @Override
     public String toString() {
-        return "[E]"+super.toString() +"(from: "+from +" to: "+to +")";
+        String date = dateTime.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+        return "[E]"+super.toString() +" (at: "+ date  + " " + from + " to " + to + ")";
     }
 }
